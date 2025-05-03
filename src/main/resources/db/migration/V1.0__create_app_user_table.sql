84997b91234cb54e4b5f5353f9e6c9020ef8c146
DubMythlit <arcadian424@gmail.com>
2025-05-03 18:49:17 +0800

    fix: 使用 app_user 作為 table name (user 是 postgres 的保留字)

--- src/main/resources/db/migration/V1.0__create_app_user_table.sql
+++ src/main/resources/db/migration/V1.0__create_app_user_table.sql
@@ -1,5 +1,5 @@
--- V1.0__create_user_table.sql
-CREATE TABLE "user" (
+-- V1.0__create_app_user_table.sql
+CREATE TABLE "app_user" (
     "id" uuid PRIMARY KEY,
     "name" character varying(255) UNIQUE,
     "password" text,
