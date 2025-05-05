1219c47f98a2973c9a19a87739a621673877f44c
DubMythlit <arcadian424@gmail.com>
2025-05-05 15:36:04 +0800

    feat: 將 auth context 提供給全 app 使用

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,34 +1,37 @@
 import React from 'react'
 import { Theme, Heading, Flex } from '@radix-ui/themes'
 import { SignupDialog } from './component/SignupDialog'
+import { AuthProvider } from './context/AuthContext'
 
 import '@radix-ui/themes/styles.css'
 import './index.css'
 
 function App() {
   return (
-    <Theme>
-      <Flex
-        direction='column'
-        height='100vh'
-      >
+    <AuthProvider>
+      <Theme>
         <Flex
-          justify='between'
-          p='2'
+          direction='column'
+          height='100vh'
         >
-          <Heading>Project Cacodemon</Heading>
-          <SignupDialog />
-        </Flex>
+          <Flex
+            justify='between'
+            p='2'
+          >
+            <Heading>Project Cacodemon</Heading>
+            <SignupDialog />
+          </Flex>
 
-        <Flex
-          align='center'
-          justify='center'
-          flexGrow='1'
-        >
-          工事中👷
+          <Flex
+            align='center'
+            justify='center'
+            flexGrow='1'
+          >
+            工事中👷
+          </Flex>
         </Flex>
-      </Flex>
-    </Theme>
+      </Theme>
+    </AuthProvider>
   )
 }
 
