797ae58f8fee911c2575a5a11cc78419f3123df0
DubMythlit <arcadian424@gmail.com>
2025-05-05 18:21:11 +0800

    feat: MemberSection 串接 get me api 取得使用者資料

--- frontend/src/component/MemberSection.jsx
+++ frontend/src/component/MemberSection.jsx
@@ -1,17 +1,45 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { Button, Flex } from '@radix-ui/themes'
 
 import { LoginDialog } from './LoginDialog'
 import { SignupDialog } from './SignupDialog'
 import { useAuth } from '../hook/useAuth'
+import { axios } from '../api'
 
 export function MemberSection() {
-  const { isAuthenticated, logout } = useAuth()
+  const { isAuthenticated, logout, token } = useAuth()
+  const [displayName, setDisplayName] = useState('')
+
+  useEffect(() => {
+    if (!isAuthenticated) {
+      return
+    }
+
+    axios.get('/api/user/me', {
+      headers: {
+        Authorization: `Bearer ${token}`
+      }
+    }).then((response) => {
+      if (response.status !== 200) {
+        logout()
+        return
+      }
+
+      setDisplayName(response.data.displayName)
+    }).catch(console.error)
+  }, [isAuthenticated])
+
   if (isAuthenticated) {
     return (
-      <Button onClick={logout}>
-        登出
-      </Button>
+      <Flex
+        align='center'
+        gap='2'
+      >
+        {`歡迎回來🎉 ${displayName}`}
+        <Button onClick={logout}>
+          登出
+        </Button>
+      </Flex>
     )
   }
 
