47da9bb166edd76884b40dd1b0c5c5064fb39ad6
DubMythlit <arcadian424@gmail.com>
2025-05-05 11:34:44 +0800

    feat: jwt token 的相關 util

---
+++ src/main/java/idv/mythlit/cacodemon/util/JwtUtil.java
@@ -0,0 +1,29 @@
+package idv.mythlit.cacodemon.util;
+
+import io.jsonwebtoken.*;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.stereotype.Component;
+
+import java.security.Key;
+import java.util.Date;
+
+@Component
+public class JwtUtil {
+    private static final Key key = Keys.secretKeyFor(SignatureAlgorithm.HS256);
+    private static final long EXPIRATION = 1000L * 60 * 60 * 24 * 30; // 1 month
+
+    public static String generateToken(String username) {
+        return Jwts.builder()
+                .setSubject(username)
+                .setExpiration(new Date(System.currentTimeMillis() + EXPIRATION))
+                .signWith(key)
+                .compact();
+    }
+
+    public static String validateToken(String token) {
+        return Jwts.parserBuilder().setSigningKey(key).build()
+                .parseClaimsJws(token)
+                .getBody()
+                .getSubject();
+    }
+}
