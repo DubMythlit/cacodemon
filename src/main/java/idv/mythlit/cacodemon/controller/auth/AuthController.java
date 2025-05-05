9e2458c0b43d8284ca84f0bbbe73305dd091cdcb
DubMythlit <arcadian424@gmail.com>
2025-05-05 12:20:24 +0800

    feat: 後端 login api

---
+++ src/main/java/idv/mythlit/cacodemon/controller/auth/AuthController.java
@@ -0,0 +1,35 @@
+package idv.mythlit.cacodemon.controller.auth;
+
+import idv.mythlit.cacodemon.service.AppUserService;
+import idv.mythlit.cacodemon.util.JwtUtil;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
+
+@RestController
+@RequestMapping("/api")
+public class AuthController {
+    private final AppUserService appUserService;
+
+    public AuthController(AppUserService appUserService) {
+        this.appUserService = appUserService;
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<?> login(@RequestBody Map<String, String> body) {
+        String username = body.get("username");
+        String password = body.get("password");
+        boolean validated = appUserService.validateAppUser(username, password);
+        if (validated) {
+            String token = JwtUtil.generateToken(username);
+            return ResponseEntity.ok(Map.of("token", token));
+        } else {
+            return ResponseEntity.status(HttpStatus.FORBIDDEN).build();
+        }
+    }
+}
