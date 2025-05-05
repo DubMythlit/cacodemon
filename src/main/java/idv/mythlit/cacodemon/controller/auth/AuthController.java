f501749d07cb68fb5eb231f6ad2429caba31adfd
DubMythlit <arcadian424@gmail.com>
2025-05-05 13:26:54 +0800

    feat: auth 統一使用 /api/auth 這個路徑

--- src/main/java/idv/mythlit/cacodemon/controller/auth/AuthController.java
+++ src/main/java/idv/mythlit/cacodemon/controller/auth/AuthController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.Map;
 
 @RestController
-@RequestMapping("/api")
+@RequestMapping("/api/auth")
 public class AuthController {
     private final AppUserService appUserService;
 
