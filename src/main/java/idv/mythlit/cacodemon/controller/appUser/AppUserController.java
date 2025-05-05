b6e5415c7de1076f2e0b3b633b923db31c843c0b
DubMythlit <arcadian424@gmail.com>
2025-05-05 15:00:54 +0800

    feat: get me api

--- src/main/java/idv/mythlit/cacodemon/controller/appUser/AppUserController.java
+++ src/main/java/idv/mythlit/cacodemon/controller/appUser/AppUserController.java
@@ -1,11 +1,18 @@
 package idv.mythlit.cacodemon.controller.appUser;
 
+import idv.mythlit.cacodemon.model.AppUser;
 import idv.mythlit.cacodemon.service.AppUserService;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Map;
+import java.util.Optional;
 
 @Controller
 @RequestMapping("/api/user")
@@ -33,5 +40,18 @@ public class AppUserController {
         } else {
             return ResponseEntity.internalServerError().build();
         }
+    }
+
+    @GetMapping("/me")
+    public ResponseEntity<Map<String, String>> me(Authentication authentication) {
+        Optional<AppUser> userOptional = appUserService.getAppUserByName(authentication.getName());
+        if (userOptional.isEmpty()) {
+            return ResponseEntity.status(HttpStatus.FORBIDDEN).build();
+        }
+        Map<String, String> resBody = Map.of(
+                "username", authentication.getName(),
+                "displayName", userOptional.get().getDisplayName()
+        );
+        return ResponseEntity.ok(resBody);
     }
 }
