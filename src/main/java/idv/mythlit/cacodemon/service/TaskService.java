cf4682122948ae858f31263a04258c991b4fca3d
DubMythlit <arcadian424@gmail.com>
2025-05-06 17:34:52 +0800

    feat: 新增 TaskService

---
+++ src/main/java/idv/mythlit/cacodemon/service/TaskService.java
@@ -0,0 +1,32 @@
+package idv.mythlit.cacodemon.service;
+
+import idv.mythlit.cacodemon.model.Task;
+import idv.mythlit.cacodemon.repository.TaskRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+import java.util.UUID;
+
+@Service
+public class TaskService {
+    private final TaskRepository taskRepository;
+
+    public TaskService(TaskRepository taskRepository) {
+        this.taskRepository = taskRepository;
+    }
+
+    public Optional<Task> createTask(String taskName, Integer pomodoroGoal) {
+        Task task = new Task();
+        task.setTaskName(taskName);
+        task.setPomodoroGoal(pomodoroGoal);
+        task.setPomodoroSpent(0);
+        UUID uuid = UUID.randomUUID();
+        task.setId(uuid.toString());
+        try {
+            taskRepository.save(task);
+            return Optional.of(task);
+        } catch (Exception e) {
+            return Optional.empty();
+        }
+    }
+}
