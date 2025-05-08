a5a599eedce602a65c524bd36174dafd53b63eda
DubMythlit <arcadian424@gmail.com>
2025-05-08 16:21:30 +0800

    feat: 實作 CurrentTask 元件功能

--- frontend/src/component/CurrentTask.jsx
+++ frontend/src/component/CurrentTask.jsx
@@ -1,13 +1,61 @@
 import React from 'react'
+import { Button, Flex, Text } from '@radix-ui/themes'
 import { useCurrentTask } from '../hook/useCurrentTask'
+import { PomodoroGauge } from './PomodoroGauge'
 
 export function CurrentTask() {
-  const { currentTask } = useCurrentTask()
+  const { currentTask, setCurrentTask } = useCurrentTask()
 
   return (
-    <div className='w-[400px] h-[80px] rounded-xl bg-gray-300'>
-      Current Task
-      {`${currentTask}`}
-    </div>
+    <Flex
+      className='w-[400px] rounded-xl bg-gray-200 p-2'
+      direction='column'
+    >
+      <CardDiscription active={currentTask !== null} />
+      {currentTask && (
+        <CurrentTaskCard
+          task={currentTask}
+          onCancel={() => setCurrentTask(null)}
+        />
+      )}
+    </Flex>
+  )
+}
+
+function CardDiscription({ active }) {
+  if (active) {
+    return (
+      <Text>目前專注的任務</Text>
+    )
+  } else {
+    return (
+      <Text align='center'>
+        目前沒有設定任務
+      </Text>
+    )
+  }
+}
+
+function CurrentTaskCard({ task, onCancel }) {
+  return (
+    <Flex
+      className='rounded-xl bg-slate-50 p-4'
+      justify='between'
+      align='center'
+    >
+      <div>
+        <Text>{task.taskName}</Text>
+        <PomodoroGauge
+          pomodoroGoal={task.pomodoroGoal}
+          pomodoroSpent={task.pomodoroSpent}
+        />
+      </div>
+      <Button
+        variant='outline'
+        onClick={onCancel}
+      >
+        取消
+      </Button>
+    </Flex>
   )
 }
